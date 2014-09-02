515709f93760a3ad42ef7cc184c1a840ab77377c
Stefan Varbanov <stefanvarbanoff@abv.bg>
2014-09-02 12:55:47 +0300

    MySql Data Access fluent model added

---
+++ TrollStore/MySqlTrollStoreModel/MySqlProductModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySqlTrollStoreModel
+{
+    public class MySqlProductModel
+    {
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Manufacturer{ get; set; }
+
+        public string Quantity { get; set; }
+        public string Store { get; set; }
+
+    }
+}
