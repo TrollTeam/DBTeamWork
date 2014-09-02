c79ead4818d18d4e5f19d511a658064ccdc4c4b9
icom85 <h.merdjanov@gmail.com>
2014-09-02 15:48:19 +0300

    Methods in MongoDb client for populating data from Products. ProductTypes, and Stores

--- TrollStore/TrollStore.Data/MongoDb/MongoDbInjector.cs
+++ TrollStore/TrollStore.Data/MongoDb/MongoDbInjector.cs
@@ -29,6 +29,8 @@
             this.AddCountries();
             this.AddManufacturers();
             this.AddStores();
+            this.AddProductTypes();
+            this.AddProducts();
         }
 
         private void AddCountries()
@@ -44,8 +46,10 @@
             {
                 this.trollStoreData.Countries.Add(new Country()
                 {
-                    CountryId = int.Parse(country["id"].ToString()),
-                    Name = country["name"].ToString()
+                    CountryId = int.Parse(country["CountryId"].ToString()),
+                    Name = country["Name"].ToString(),
+                    CountryCode = country["CountryCode"].ToString(),
+                    Currency = country["Currency"].ToString()
                 });
             }
 
@@ -65,9 +69,9 @@
             {
                 this.trollStoreData.Manufacturers.Add(new Manufacturer()
                 {
-                    ManufacturerId = int.Parse(manufacturer["id"].ToString()),
-                    Name = manufacturer["name"].ToString(),
-                    CountryId = int.Parse(manufacturer["countryId"].ToString())
+                    ManufacturerId = int.Parse(manufacturer["ManufacturerId"].ToString()),
+                    Name = manufacturer["Name"].ToString(),
+                    CountryId = int.Parse(manufacturer["CountryId"].ToString())
                 });
             }
 
@@ -76,7 +80,7 @@
 
         private void AddStores()
         {
-            //if (this.trollStoreData.Manufacturers.All().Any())
+            //if (this.trollStoreData.Stores.All().Any())
             //{
             //    return;
             //}
@@ -87,10 +91,63 @@
             {
                 this.trollStoreData.Stores.Add(new Store()
                 {
-                    StoreId = int.Parse(store["id"].ToString()),
-                    Name = store["name"].ToString(),
-                    CountryId = int.Parse(store["countryId"].ToString())
+                    StoreId = int.Parse(store["StoreId"].ToString()),
+                    Name = store["Name"].ToString(),
+                    CountryId = int.Parse(store["CountryId"].ToString())
                 });
+            }
+
+            this.SaveChanges();
+        }
+
+        private void AddProductTypes()
+        {
+            //if (this.trollStoreData.ProductTypes.All().Any())
+            //{
+            //    return;
+            //}
+
+            var productTypes = this.mongoDbCloud.GetItemsFromCollection("producttypes");
+
+            foreach (var productType in productTypes)
+            {
+                this.trollStoreData.ProductTypes.Add(new ProductType()
+                {
+                    ProductTypeId = int.Parse(productType["ProductTypeId"].ToString()),
+                    Name = productType["Name"].ToString()
+                });
+            }
+
+            this.SaveChanges();
+        }
+
+        private void AddProducts()
+        {
+            //if (this.trollStoreData.Products.All().Any())
+            //{
+            //    return;
+            //}
+
+            var products = this.mongoDbCloud.GetItemsFromCollection("products");
+
+            foreach (var product in products)
+            {
+                var newProduct = new Product()
+                {
+                    ProductId = int.Parse(product["ProductId"].ToString()),
+                    Name = product["Name"].ToString(),
+                    Quantity = int.Parse(product["Quantity"].ToString()),
+                    PriceDelivered = int.Parse(product["PriceDelivered"].ToString()),
+                    PriceSold = int.Parse(product["PriceSold"].ToString()),
+                    ProductTypeId = int.Parse(product["ProductTypeId"].ToString()),
+                    ManufacturerId = int.Parse(product["ManufacturerId"].ToString())
+                };
+
+                this.trollStoreData.Products.Add(newProduct);
+                var storeId = int.Parse(product["StoreId"].ToString());
+                var storeForProduct = this.trollStoreData.Stores.All().First(s => s.StoreId == storeId);
+
+                storeForProduct.Products.Add(newProduct);
             }
 
             this.SaveChanges();
