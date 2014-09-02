a060ad1dfbf8048995d5fe72e5108efe781911c1
Ani_Mihaylova <ani.ad.lib@gmail.com>
2014-09-02 16:53:01 +0300

    AddSQLite project

---
+++ TrollStore/SQLite/ProductData/SQLiteProductContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLiteDatabase.Models;
+
+namespace SQLiteDatabase.Data
+{
+    public class SQLiteProductContext : DbContext
+    {
+        public SQLiteProductContext()
+            : base(@"Data Source=..\\..\\ProductsDatabase\SQLiteDatabase.sqlite; Version=3;")
+        {
+            Database.SetInitializer<SQLiteProductContext>(null);
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
